ba1c7940cf54506b08afa19e895f2c7ce8d2c9ef
Meenakshi Rajakumar <meenagayu19@gmail.com>
2021-07-19 15:46:42 +0100

    Functions in Story 3

--- Story_3/Story_3.ino
+++ Story_3/Story_3.ino
@@ -17,20 +17,8 @@ void setup()
 
 void loop()
 {
-  digitalWrite(RIGHTENABLE, HIGH);
-  digitalWrite(RIGHTFORWARD, HIGH); 
-  digitalWrite(RIGHTREVERSE, LOW);
-  digitalWrite(LEFTENABLE, HIGH);
-  digitalWrite(LEFTFORWARD, HIGH);
-  digitalWrite(LEFTREVERSE, LOW);  
-  delay(5000); // Wait for 5000 millisecond(s)
-  digitalWrite(RIGHTENABLE, LOW);
-  digitalWrite(RIGHTFORWARD, LOW);
-  digitalWrite(RIGHTREVERSE, LOW);
-  digitalWrite(LEFTENABLE, LOW);
-  digitalWrite(LEFTFORWARD, LOW);
-  digitalWrite(LEFTREVERSE, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  forward(5);
+  Stop(1);
   digitalWrite(RIGHTENABLE, HIGH);
   digitalWrite(RIGHTFORWARD, LOW);
   digitalWrite(RIGHTREVERSE, HIGH);
@@ -38,4 +26,36 @@ void loop()
   digitalWrite(LEFTFORWARD, LOW);
   digitalWrite(LEFTREVERSE, HIGH);  
   delay(2000); // Wait for 2000 millisecond(s)
+}
+
+void forward(int seconds)
+{
+  digitalWrite(RIGHTENABLE, HIGH);
+  digitalWrite(RIGHTFORWARD, HIGH); 
+  digitalWrite(RIGHTREVERSE, LOW);
+  digitalWrite(LEFTENABLE, HIGH);
+  digitalWrite(LEFTFORWARD, HIGH);
+  digitalWrite(LEFTREVERSE, LOW); 
+  delay(seconds * 1000);
+}
+
+void Stop(int seconds)
+{
+  digitalWrite(RIGHTENABLE, LOW);
+  digitalWrite(RIGHTFORWARD, LOW);
+  digitalWrite(RIGHTREVERSE, LOW);
+  digitalWrite(LEFTENABLE, LOW);
+  digitalWrite(LEFTFORWARD, LOW);
+  digitalWrite(LEFTREVERSE, LOW); 
+  delay(seconds * 1000);
+}
+void reverse(int seconds)
+{
+  digitalWrite(RIGHTENABLE, HIGH);
+  digitalWrite(RIGHTFORWARD, LOW);
+  digitalWrite(RIGHTREVERSE, HIGH);
+  digitalWrite(LEFTENABLE, HIGH);
+  digitalWrite(LEFTFORWARD, LOW);
+  digitalWrite(LEFTREVERSE, HIGH); 
+  delay(seconds * 1000);
 }
